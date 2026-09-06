a5f03ff24b27d24c6c8d2ca56addc4a8fe059064
Krithik Naidu <nadiukrithik37@gmail.com>
2026-09-07 01:56:42 +0530

    dashboard complete

--- components/custom/dashboard/ProjectList.tsx
+++ components/custom/dashboard/ProjectList.tsx
@@ -1,8 +1,29 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
+import Image from 'next/image';
+import { Button } from '@/components/ui/button';
 
 function ProjectList() {
+
+  const [ProjectList, setProjectList] = useState([]);
+
   return (
-    <div>ProjectList</div>
+    <div>
+      {ProjectList.length === 0 ? (
+        <div className='flex flex-col items-center p-10 gap-4 mt-8 border-2 rounded-xl p-10'>
+          <Image src="/image.png" alt="Folder Image" width={50} height={50} />
+          <div className='text-center'>
+            <h1 className='text-2xl font-bold'>No Board Found</h1>
+            <p className='text-muted-foreground mb-2'>Create Your First board, start planning!</p>
+            <Button>+ Create Project</Button>
+          </div>
+        </div>
+      ) :
+        <div>
+          {/* {Project List}</div>} */}
+        </div>
+      }
+    </div>
   )
 }
 
