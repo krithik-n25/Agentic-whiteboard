7d119391b06260c5ccd6a24dd44ba025a75fe8ae
Krithik Naidu <nadiukrithik37@gmail.com>
2026-09-08 00:50:35 +0530

    Create a new whiteboard space

--- app/api/projects/route.ts
+++ app/api/projects/route.ts
@@ -3,18 +3,18 @@ import { currentUser } from "@clerk/nextjs/server";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req:NextRequest){
-    const {project_name,project_id}=await req.json();
+    const {projectName,projectId}=await req.json();
 
     const user =await currentUser();
 
-    if(!project_id || !project_name){
+    if(!projectId || !projectName){
         return NextResponse.json("Missing required fields",{status:400})
     }
 
     const result = await db.insert(Projects).values({
-        projectId:project_id,
-        projectName:project_name??'',
-        userEmail:user?.primaryEmailAddress?.emailAddress??'',
+        projectId:projectId,
+        projectName:projectName??'',
+        userEmail:user?.primaryEmailAddress?.emailAddress??''
     }).returning();
 
     return NextResponse.json(result[0])
